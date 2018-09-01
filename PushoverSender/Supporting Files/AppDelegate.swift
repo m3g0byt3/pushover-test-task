80a2b35069c9ca76ede804b2574ebe526796378e
m3g0byt3 <m3g0byt3@gmail.com>
2018-09-01 13:08:12 +0300

    Initial commit

---
+++ PushoverSender/Supporting Files/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  PushoverSender
+//
+//  Created by m3g0byt3 on 01/09/2018.
+//  Copyright © 2018 m3g0byt3. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
+    ) -> Bool {
+        return true
+    }
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+PushoverSender
+=======
+
+Send notifications using [Pushover](https://pushover.net/) API.
+
+Requirements
+----------------
+* iOS 9.3+
+* Xcode 9.0+
+* Swift 4.0+
+
+Installation
+----------------
+
+```
+git clone https://github.com/m3g0byt3/PushoverSender.git\
+ && cd PushoverSender\
+  && pod install\
+   && open PushoverSender.xcworkspace
+```
+
+Author
+----------------
+[m3g0byt3](https://github.com/m3g0byt3)
