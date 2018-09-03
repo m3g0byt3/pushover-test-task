467c55b8495e80087516efc46a0322197f7e0c75
m3g0byt3 <m3g0byt3@gmail.com>
2018-09-03 15:00:22 +0300

    Add custom Alamofire manager with short timeouts

---
+++ PushoverSender/Extensions/MoyaProvider+Extensions.swift
@@ -0,0 +1,30 @@
+//
+//  MoyaProvider+Extensions.swift
+//  PushoverSender
+//
+//  Created by m3g0byt3 on 03/09/2018.
+//  Copyright © 2018 m3g0byt3. All rights reserved.
+//
+
+import Foundation
+import Moya
+import Alamofire
+
+/// Provides custom AlamofireManager
+public extension MoyaProvider {
+
+    /// Class method that returns customized instance of Alamofire default manager
+    /// - returns: Customized instance of Alamofire default manager
+    public final class func customAlamofireManager() -> Manager {
+        let configuration = URLSessionConfiguration.default
+        configuration.timeoutIntervalForRequest = Constants.Network.timeout
+        configuration.timeoutIntervalForResource = Constants.Network.timeout
+        configuration.httpAdditionalHeaders = Manager.defaultHTTPHeaders
+        configuration.requestCachePolicy = .reloadIgnoringLocalAndRemoteCacheData
+
+        let manager = Manager(configuration: configuration)
+        manager.startRequestsImmediately = false
+
+        return manager
+    }
+}
